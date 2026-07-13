80f11efe2da3c94963391f60a49e4a403c2bb0b1
martins091 <agbomartinsejiofor@gmail.com>
2026-07-13 15:43:09 +0100

    remove build files

--- out/_next/static/1n5S5r3qnd_L5Su4aAjRv/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcase-studies\u002F[slug]","\u002Findustries\u002F[slug]","\u002Fnews\u002F[slug]","\u002Fproducts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
